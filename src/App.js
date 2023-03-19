ce5287b31e935e811426fa86cfcca9bec85280bf
iqbalf06 <faisaelz@gmail.com>
2023-03-19 10:22:18 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
+  const hello = "Hello World"
+  function handleClick() {
+    alert('Hello')
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {hello}
         </p>
+        <Button variant='primary' onClick={handleClick}>Hello button</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
